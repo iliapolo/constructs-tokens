e2499fc2bbe0b71c5c8e9f13c56677ed5ee888f6
Eli Polonsky <epolon@amazon.com>
2020-07-27 14:24:13 +0300

    rename to scope level package

--- .projenrc.js
+++ .projenrc.js
@@ -3,7 +3,7 @@ const { JsiiProject } = require('projen');
 const project = new JsiiProject({
   "authorName": "Eli Polonsky",
   "authorEmail": "eli.polonsky@gmail.com",
-  "name": "constructs-tokens",
+  "name": "@iliapolo/constructs-tokens",
   "repository": "https://github.com/iliapolo/constructs-tokens"
 });
 
