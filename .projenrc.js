1d4f5edd056011cfdc5255394a78010ecf0ebdaa
Eli Polonsky <epolon@amazon.com>
2020-07-27 23:15:13 +0300

    rename to -staging

--- .projenrc.js
+++ .projenrc.js
@@ -3,7 +3,7 @@ const { JsiiProject, Semver } = require('projen');
 const project = new JsiiProject({
   authorName: "Eli Polonsky",
   authorEmail: "eli.polonsky@gmail.com",
-  name: "@iliapolo/constructs-tokens",
+  name: "constructs-tokens-staging",
   repository: "https://github.com/iliapolo/constructs-tokens",
   dependencies: {
     'constructs': Semver.caret('3.0.4')
